b2857d1e3211732da0e048e3e57ddc894a8c7b59
mac91p <mackobws@gmail.com>
2023-09-03 21:33:25 +0200

    Change querying way

--- src/main/java/pl/kurs/finaltest/services/ShapeManagementService.java
+++ src/main/java/pl/kurs/finaltest/services/ShapeManagementService.java
@@ -3,7 +3,6 @@ package pl.kurs.finaltest.services;
 import org.springframework.stereotype.Service;
 import pl.kurs.finaltest.models.*;
 import pl.kurs.finaltest.respositories.ShapeRepository;
-import pl.kurs.finaltest.respositories.predicates.IShapePredicate;
 import java.util.List;
 import java.util.Map;
 
@@ -11,12 +10,10 @@ import java.util.Map;
 public class ShapeManagementService extends AbstractGenericManagementService<Shape, ShapeRepository> {
 
 
-    private final List<IShapePredicate> shapePredicates;
     private ShapeSpecificationService shapeSpecificationService;
 
-    public ShapeManagementService(ShapeRepository repository, List<IShapePredicate> shapePredicates, ShapeSpecificationService shapeSpecificationService) {
+    public ShapeManagementService(ShapeRepository repository, ShapeSpecificationService shapeSpecificationService) {
         super(repository);
-        this.shapePredicates = shapePredicates;
         this.shapeSpecificationService = shapeSpecificationService;
     }
 
